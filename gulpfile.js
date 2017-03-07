a029617c224fcad0dc0096d93c6f01f96abf531c
Synthiaj <synthiajacob.pfa@gmail.com>
2017-03-07 22:12:03 +0200

    push

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglifyjs');
+//***********
+//
+// Watchable files
+//
+//***********
+
+//
+//SASS
+//
+gulp.task('sass', function() {
+    return gulp.src('./src/scss/style.min.scss')
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./css'));
+});
+//
+// Javascript
+//
+
+//custom
+gulp.task('compressjSCustom', function() {
+    return gulp.src('./src/javascript/custom/*.js')
+        .pipe(uglify('custom.min.js', {
+            outSourceMap: true,
+            mangle: false
+        }))
+        .pipe(gulp.dest('./js'));
+});
+//custom
+gulp.task('compressjSLib', function() {
+    return gulp.src('./src/javascript/lib/*.js')
+        .pipe(uglify('lib.min.js', {
+            outSourceMap: true,
+            mangle: false
+        }))
+        .pipe(gulp.dest('./js'));
+});
+//
+// First run files
+//
+
+gulp.task('sass:watch', function() {
+    gulp.watch('./src/scss/custom/*.scss', ['sass']);
+    gulp.watch('./src/scss/*/*.scss', ['sass']);
+    gulp.watch('./src/javascript/lib/*.js', ['compressjSLib']);
+    gulp.watch('./src/javascript/custom/*.js', ['compressjSCustom']);
+
+});
+gulp.task('default', ['sass', 'compressjSLib', 'compressjSCustom', 'sass:watch']);
---
+++ src/javascript/custom/script.js
@@ -0,0 +1,65 @@
+//the angular part
+var ppdMod = angular.module('ppdAng', []);
+ppdMod.controller('ppdControler', ['$scope', function($scope) {
+
+    $scope.grid = [
+        ['Empty']
+    ];
+    $scope.sideColumn = [];
+    $scope.rowIndex = -1;
+
+    $scope.addRow = function() {
+        var newGenArr = $scope.grid;
+        newGenArr.forEach(function(value, index) {
+            value.push('Empty');
+        });
+        $scope.sideColumn.push('Empty Row Description');
+
+        $scope.grid = newGenArr;
+    };
+    $scope.deleteRow = function(key) {
+        var newGenArr = $scope.grid;
+        newGenArr.forEach(function(value, index) {
+            value.splice(key, 1);
+        });
+        $scope.sideColumn.splice(key, 1);
+        $scope.grid = newGenArr;
+    };
+    $scope.addColumn = function() {
+        var newGenArr = $scope.grid;
+        if (newGenArr[0]) {
+            newGenArr.push(returnArrLen(newGenArr[0].length));
+        } else {
+            newGenArr.push(['Empty']);
+        }
+    };
+    $scope.changeRowIndex = function(ind) {
+        $scope.rowIndex = ind;
+    };
+
+    $scope.highlughtIfIndex = function(ind) {
+      console.log(ind);
+        if (ind === $scope.rowIndex) {
+            return true;
+        }
+        return false;
+
+
+    };
+    $scope.removeColumn = function(index) {
+        $scope.grid.splice(index, 1);
+    };
+    var returnArrLen = function(size) {
+        var arr = [];
+        while (arr.length < size) {
+            arr.push('Empty');
+        }
+        return arr;
+    };
+}]);
+//the jquery part
+jQuery(document).ready(function($) {
+
+
+
+});
